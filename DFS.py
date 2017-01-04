67e560c0f646b9b22267f0b646613be3b58ae6ab
Arnaud Desombre <arnauddesombre@yahoo.com>
2017-01-04 06:32:49 -0500

    Add files via upload

--- DFS.py
+++ DFS.py
@@ -1,19 +1,78 @@
 """
 explore a graph using Depth First Search (DFS)
 
-return a dictionary containing the visiting order
-for each node starting at visited[source] = 1
+compute dictionaries containing the visiting orders
+(pre_visit & post_visit) for each node starting at 1
+
+implements:
+DFS_recursive(graph, source): a recursive algorithm
+DFS_linear(graph, source): a linear algorithm
+DFS(graph, source, rec=False): which calls one or the other (default: 'rec')
 """
+
+# recursion will eventually crash for large graphs
+recursive = False
+
 import sys
 sys.setrecursionlimit(1000000)
 
-def DFS(graph, source, count=0, visited={}):
-    count += 1
-    visited[source] = count
+def init():
+    global pre_clock, post_clock, pre_visit, post_visit
+    pre_clock = 0
+    post_clock = 0
+    pre_visit = {}
+    post_visit = {}
+
+def previsit(node):
+    global pre_clock, pre_visit
+    pre_clock += 1
+    pre_visit[node] = pre_clock
+
+def postvisit(node):
+    global post_clock, post_visit
+    post_clock += 1
+    post_visit[node] = post_clock
+
+def DFS(graph, source, rec=recursive):
+    if rec:
+        return DFS_recursive(graph, source)
+    else:
+        return DFS_linear(graph, source)
+
+def DFS_recursive(graph, source, first_time=True):
+    # recursive DFS
+    if first_time:
+        init()
+    previsit(source)
     for node in graph[source]:
-        if node not in visited:
-            visited = DFS(graph, node, count, visited)
-    return visited
+        if node not in pre_visit:
+            DFS_recursive(graph, node, False)
+    postvisit(source)
+    if first_time:
+        return pre_visit, post_visit
+    else:
+        return
+
+def DFS_linear(graph, source):
+    # linear DFS
+    init()
+    for node in graph:
+        if node in post_visit:
+            continue
+        previsit(node)
+        stack = [(node, graph[node].__iter__())]
+        while stack:
+            x, iterator = stack.pop()
+            try:
+                y = next(iterator)
+                stack.append((x, iterator))
+                if y in pre_visit or y in post_visit:
+                    continue
+                previsit(y)
+                stack.append((y, graph[y].__iter__()))
+            except StopIteration:
+                postvisit(x)
+    return pre_visit, post_visit
 
 
 ############################
@@ -37,3 +96,5 @@ if __name__ == "__main__":
              'E': {'C':2, 'D':1}}
 
     print DFS(graph, 'A')
+    print DFS_recursive(graph, 'A')
+    print DFS_linear(graph, 'A')
